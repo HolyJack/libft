8a70f47363ad49bf523c109114af8725e9a14f6e
Eldon Jafer <ejafer@ox-w1.msk.21-school.ru>
2021-10-16 22:44:29 +0300

    Calloc fixed. 2 unprotected functions left.

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: ejafer <ejafer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 15:02:01 by ejafer            #+#    #+#             */
-/*   Updated: 2021/10/16 22:17:30 by ejafer           ###   ########.fr       */
+/*   Updated: 2021/10/16 22:43:10 by ejafer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,6 @@ void	*ft_calloc(size_t count, size_t size)
 	tmp = malloc(count * size);
 	if (tmp == NULL)
 		return (NULL);
-	ft_bzero(tmp, count);
+	ft_bzero(tmp, count * size);
 	return (tmp);
 }
