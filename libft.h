1b9e188e13cb469257d33135498b036922f5a42e
Eldon Jafer <ejafer@ox-v1.msk.21-school.ru>
2021-10-22 19:42:53 +0300

    ft_strccpy was deleted from the header

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ejafer <ejafer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 16:52:41 by ejafer            #+#    #+#             */
-/*   Updated: 2021/10/22 17:17:39 by ejafer           ###   ########.fr       */
+/*   Updated: 2021/10/22 19:42:12 by ejafer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,6 @@ int		ft_toupper(int c);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
-void	*ft_memccpy(
-			void *restrict dst, const void *restrict src, int c, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
