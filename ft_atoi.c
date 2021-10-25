e15ada0cfe7c5daf41134c09b4da1fb18effb788
Eldon Jafer <ejafer@ox-v1.msk.21-school.ru>
2021-10-25 18:26:42 +0300

    ft_atoi now return -1 and 0 if there is integer overflow

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,23 +6,27 @@
 /*   By: ejafer <ejafer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 15:43:42 by ejafer            #+#    #+#             */
-/*   Updated: 2021/10/18 18:30:40 by ejafer           ###   ########.fr       */
+/*   Updated: 2021/10/25 18:10:34 by ejafer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_atoi(const char *s)
+static const char	*ft_skipispace(const char *str)
 {
-	int		buf;
-	int		sign;
-	char	*str;
-
-	sign = 1;
-	str = (char *) s;
 	while (*str == ' ' || *str == '\t' || *str == '\n'
 		|| *str == '\f' || *str == '\r' || *str == '\v')
 		str++;
+	return (str);
+}
+
+int	ft_atoi(const char *str)
+{
+	long int		buf;
+	int				sign;
+
+	sign = 1;
+	str = ft_skipispace(str);
 	if (*str == '-')
 	{
 		sign = -1;
@@ -35,6 +39,10 @@ int	ft_atoi(const char *s)
 	{
 		buf *= 10;
 		buf += (*str - '0');
+		if (sign * buf < -2147483648)
+			return (0);
+		if (sign * buf > 2147483647)
+			return (-1);
 		str++;
 	}
 	return (sign * buf);
